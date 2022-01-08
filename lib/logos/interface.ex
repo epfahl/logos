0f16ec7d0152a016e961d70d7d77703d3f0e445d
Eric Pfahl <eric.pfahl@gmail.com>
2022-01-08 15:14:51 -0500

    refactored initial implementation of choice and allowed for implicit conjunction

--- lib/logos/interface.ex
+++ lib/logos/interface.ex
@@ -9,6 +9,12 @@ defmodule Logos.Interface do
   alias Logos.Interface, as: I
 
   @doc """
+  Turn a list of goals into a conjunction (via `all`). A single goal is passed through unchanged.
+  """
+  def implicit_all(goals) when is_list(goals), do: C.all(goals)
+  def implicit_all(goal), do: goal
+
+  @doc """
   Return a goal that is the disjunction over conjunction clauses, where each clause is a list of goals that represents and implicit conjunction.
   """
   defmacro fork(do: clause_block) do
@@ -16,10 +22,7 @@ defmodule Logos.Interface do
 
     quote do
       unquote(clauses)
-      |> Enum.map(fn
-        clause when is_list(clause) -> C.all(clause)
-        clause -> clause
-      end)
+      |> Enum.map(&I.implicit_all/1)
       |> C.any()
     end
   end
@@ -83,24 +86,29 @@ defmodule Logos.Interface do
   end
 
   @doc """
-
-  Notes
-  * `switch` is a normal function and can't parse an implicit conjunction.
-  * Generalize this to work with any kind of right-hand goal, and implicit conjunctions on the left?
+  "Impure" relation that evaluates the first clauses whose condition goal is successful. Implicit conjunction is allowed for both the condition and consequence clauses.
   """
-  defmacro choice(do: [choice_clause]) do
-    [g_cond, g_then] = choice_goals(choice_clause)
+  defmacro choice(do: [clause | other_clauses]) do
+    [g_cond, g_then] = choice_goals(clause)
 
+    do_choice(g_cond, g_then, other_clauses)
+  end
+
+  defp do_choice(g_cond, g_then, []) do
     quote do
-      C.all([unquote(g_cond), unquote(g_then)])
+      [unquote(g_cond), unquote(g_then)]
+      |> Enum.map(&I.implicit_all/1)
+      |> C.all()
     end
   end
 
-  defmacro choice(do: [choice_clause | t]) do
-    [g_cond, g_then] = choice_goals(choice_clause)
-
+  defp do_choice(g_cond, g_then, [_h | _t] = clauses) do
     quote do
-      C.switch(unquote(g_cond), unquote(g_then), I.choice(do: unquote(t)))
+      C.switch(
+        I.implicit_all(unquote(g_cond)),
+        I.implicit_all(unquote(g_then)),
+        I.choice(do: unquote(clauses))
+      )
     end
   end
 
